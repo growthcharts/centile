61f6e51d66bd8420ab91fc8929ea0bce80b6cad6
Stef van Buuren <stef.vanbuuren@tno.nl>
2021-02-06 17:15:48 +0100

    Extend search in load_reference() to other packages

--- R/load_reference.R
+++ R/load_reference.R
@@ -2,41 +2,46 @@
 #'
 #' This function provides access to the built-in growth references.
 #' @param refcode String, code of a reference. Only the first element is loaded.
-#' @param element String, either `"table"`, `"index"`, `"study"` or
-#' `"all"`.  The default is `"table"`.
-#' @param con A database connection. The default `con = NULL` reads the
-#' data from the internal data. Database access is currently not implemented.
+#' @param element Keyword, either `"all"`, `"table"`, `"index"` or `"study"`.
+#' The default is `"all"`.
+#' @param pkg The package containing the reference. The package must be loaded
+#' and attached. The default `pkg = "yzy"` searches in the home package.
 #' @return The return value depends on the `element` parameter.
 #'
 #' `element` | Return value
 #' --------- | ---------------------------------------
+#' `"all"`   | All stored information
 #' `"table"` | A `tibble` with reference values
 #' `"index"` | Numeric vector with index values
 #' `"study"` | Named character vector with study data
-#' `"all"`   | All stored information
 #'
 #' @examples
 #' ref <- load_reference("gc_2019_dsc_male_")
 #' @export
 load_reference <- function(refcode = NULL,
-                           element = c("table", "index", "study", "all"),
-                           con = NULL) {
-  if (!is.null(con)) {
-    warning("Database access not implemented.")
-    return(NULL)
-  }
+                           element = c("all", "table", "index", "study"),
+                           pkg = "yzy") {
   if (is.null(refcode)) {
     return(NULL)
   }
+  if (!pkg %in% loadedNamespaces()) {
+    warning("Package ", pkg, " not loaded.")
+    return(NULL)
+  }
+  ref <- get0(refcode[[1L]], envir = asNamespace(pkg))
+  if (is.null(ref)) {
+    warning("Reference ", refcode[[1L]], " not found in package ", pkg, ".")
+    return(NULL)
+  }
+
   element <- match.arg(element)
-  ref <- get0(refcode[[1L]])
   switch(element,
+    all = ref,
     table = {
       attr(ref, "study") <- NULL
       ref
     },
     index = ref[["x"]],
-    study = attr(ref, "study"),
-    all = ref
+    study = attr(ref, "study")
   )
 }
---
+++ tests/testthat/test-load_reference.R
@@ -0,0 +1,24 @@
+
+test_that("finds internal reference", {
+  expect_s3_class(load_reference("who_2011_hgt_male_"), "tbl")
+})
+
+test_that("finds internal reference: table", {
+  expect_s3_class(load_reference("who_2011_hgt_male_", "table"), "tbl")
+})
+
+test_that("finds internal reference: study", {
+  expect_type(load_reference("who_2011_hgt_male_", "study"), "character")
+})
+
+test_that("finds internal reference: index", {
+  expect_type(load_reference("who_2011_hgt_male_", "index"), "double")
+})
+
+test_that("cannot find invalid refcode", {
+  expect_warning(load_reference("junk"))
+})
+
+test_that("cannot find invalid pkg", {
+  expect_warning(load_reference("junk", pkg = "notloaded"))
+})
