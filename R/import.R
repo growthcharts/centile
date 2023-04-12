3c3ff92a6399b5deb2413a6395027808946cc275
Stef van Buuren <stef.vanbuuren@tno.nl>
2023-04-12 12:59:19 +0200

    Replace BCT by more robust internal version

--- R/import.R
+++ R/import.R
@@ -1,7 +1,7 @@
 #' @importFrom dplyr       %>% first group_by mutate pull ungroup
-#' @importFrom gamlss.dist pBCCG pBCPE pBCT qBCCG qBCPE qBCT
+#' @importFrom gamlss.dist pBCCG pBCPE qBCCG qBCPE
 #' @importFrom readr       cols col_double parse_character
 #'                         read_delim read_lines read_tsv
 #' @importFrom rlang       .data
-#' @importFrom stats       approx qnorm pnorm
+#' @importFrom stats       approx qnorm pnorm pt qt
 NULL
